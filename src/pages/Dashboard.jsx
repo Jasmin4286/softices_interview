d39073bf4ac9044a0f31e71885bedb409c6c1142
sanganiviraj <sanganiviraj263@gmail.com>
2024-02-08 13:46:30 +0530

    hello msg print

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,11 +2,24 @@ import moment from "moment/moment";
 import React from "react";
 
 const Dashboard = () => {
-  const myDate = new Date().toDateString();
-  // console.log("myDate=======>", myDate);
+  const myDate = new Date().toTimeString();
+  const getHours = myDate.split(":")[0];
+
+  const morningMsg = () => {
+    if (getHours <= 12) {
+      return "Good morning";
+    } else if (getHours <= 17) {
+      return "Good Afternoon";
+    } else if (getHours <= 20) {
+      return "Good Evening";
+    } else {
+      return "Good Night";
+    }
+  };
   return (
     <>
       <h1>Dashboard</h1>
+      {`${morningMsg()} Jasmin Vaghasiya`}
     </>
   );
 };
